ae9ba81500c4147dd58f3bb7de0050098dfe9441
pocketclint <118079528+pocketclint@users.noreply.github.com>
2023-03-13 21:14:52 -0500

    modifications to get server.js functioning

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 
-const sequelize = require('./config/connection');
+const sequelize = require('./config/connections');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -23,7 +23,7 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
