fbfc7f677e70f81e0b98ff47eb34b26882fe2dbc
gaughanln <gaughanln@yahoo.com>
2023-03-16 18:05:45 -0500

    started readme

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,3 @@
-const accessToken = 'BQAZJtYXuyt27N7lYT1OEtxWEwMdnB3OJhbfb8BB7qzi-40o8Up0JWUItBf2I3K7DcRbZD9PtL1s0Vu9KvInubLHPRv1BVgiq21PHB-JXwLS3EI887tfrAw5SpN-_VvL-F4m5PLrXTmY0bERnNdQbwal8swxfEChLn3t2aS3xewLLQ'
+// last.fm api
 
-fetch('https://api.spotify.com/v1/playlists/3kIykIdSSGZcgU7NsA2aZA', {
-  headers: {
-    Authorization: `Bearer ${accessToken}`,
-  },
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error(error));
+// http://www.last.fm/api/auth/?api_key=abb32851ff5747ae387a533a599062a3
