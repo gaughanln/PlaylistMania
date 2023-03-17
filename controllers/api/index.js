c9368919b5d128b8c97d5dd2ae7ef7ef8ea5374a
pocketclint <118079528+pocketclint@users.noreply.github.com>
2023-03-16 20:07:11 -0500

    it never ends

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
+
 const userRoutes = require('./userRoutes');
 
-const homepage = require('./homepage');
-
 router.use('/users', userRoutes);
-router.use('/app', homepage);
 
 module.exports = router;
--- controllers/homepage.js
+++ controllers/homepage.js
@@ -4,7 +4,6 @@ const express = require('express');
 const router = express.Router();
 
 
-
 router.use(express.static('public'));
 
 router.get('/', (req, res) => {
