6bdfac9d9d20772872a0bb9a59048e72491ef4bc
Jason Shi <jshigoodies@gmail.com>
2023-03-19 15:59:08 -0500

    added so you can't go to the /artist even if logged out

---
+++ public/js/logout.js
@@ -0,0 +1,10 @@
+const logoutButton = document.querySelector('#logout');
+
+logoutButton.addEventListener('click', function(event) {
+    fetch('/api/users/logout', {
+        method: 'POST'
+    });
+
+    console.log("session should end");
+
+});
