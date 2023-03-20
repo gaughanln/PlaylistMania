52e2730d5465af8077d7d6f47756425b04e92537
pocketclint <118079528+pocketclint@users.noreply.github.com>
2023-03-19 23:45:08 -0500

    connection

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const session = require('express-session');
 const routes = require('./controllers');
 
-const sequelize = require('./config/connections');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
