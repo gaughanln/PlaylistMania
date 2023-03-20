900ede34050b50675df4d70edf3f51fdfc8a33ba
pocketclint <118079528+pocketclint@users.noreply.github.com>
2023-03-19 23:11:59 -0500

    one of these days

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const sequelize = require('./config/connections');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
 
